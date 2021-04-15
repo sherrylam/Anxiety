c902185ee6a617b51ad6252fe892390bb6c13e63
Sherry Lam <sherrylam2@gmail.com>
2021-04-15 06:56:22 -0700

    added relief section

--- sketch.js
+++ sketch.js
@@ -9,8 +9,11 @@ var npcSprite;
 var adventureManager;
 
 // clickables
-var clickablesManager;    // the manager class
-var clickables;   
+var clickablesManager;
+var clickables; 
+
+// indexes into the clickable array (constants)
+const playGameIndex = 0;  
 
 function preload() {
   // loads all the image files
@@ -18,10 +21,14 @@ function preload() {
   npcanimation = loadAnimation('assets/npc_sprite1.png', 'assets/npc_sprite4.png')
 
   adventureManager = new AdventureManager('data/adventureStates.csv', 'data/interactionTable.csv');
+  clickablesManager = new ClickableManager('data/clickableLayout.csv');
 }
 
 function setup() {
   createCanvas(1000, 800);
+
+  // setup the clickables = this will allocate the array
+  clickables = clickablesManager.setup();
   
   // change how many frames (of draw loop) each animation frame is
   // visible for. bigger #s = slower speed
@@ -38,14 +45,24 @@ function setup() {
   // use this to track movement from toom to room in adventureManager.draw()
   adventureManager.setPlayerSprite(playerSprite);
 
+  // based on the state name in the clickableLayout
+  adventureManager.setClickableManager(clickablesManager);
+
     // This will load the images, go through state and interation tables, etc
   adventureManager.setup();
+
+  // call OUR function to setup additional information about the p5.clickables
+  // that are not in the array 
+  setupClickables();
 }
 
 function draw() { 
   background(220);
   // draws background rooms and handles movement from one to another
   adventureManager.draw();
+
+  // draw the p5.clickables, in front of the mazes but behind the sprites 
+  clickablesManager.draw();
   
   if( adventureManager.getStateName() !== "Splash" &&
       adventureManager.getStateName() !== "Instructions") {
@@ -57,7 +74,9 @@ function draw() {
     drawSprite(playerSprite);
   } 
 
-  if( adventureManager.getStateName() == "Chaos") {
+  if( adventureManager.getStateName() == "Chaos" |
+      adventureManager.getStateName() == "ChaosTalk" |
+      adventureManager.getStateName() == "Relief") {
     drawSprite(npcSprite);
   }
 }
@@ -101,4 +120,34 @@ function moveSprite() {
   else {
     playerSprite.velocity.y = 0;
   }
+}
+
+//-------------- CLICKABLE CODE  ---------------//
+
+function setupClickables() {
+  // All clickables to have same effects
+  for( let i = 0; i < clickables.length; i++ ) {
+    clickables[i].onHover = clickableButtonHover;
+    clickables[i].onOutside = clickableButtonOnOutside;
+    clickables[i].onPress = clickableButtonPressed; 
+  }
+}
+
+// tint when mouse is over
+clickableButtonHover = function () {
+  this.color = "#F7F7F7";
+  this.noTint = false;
+  this.tint = "#F7F7F7";
+}
+
+// color a light gray if off
+clickableButtonOnOutside = function () {
+  // backto our gray color
+  this.color = "#F7F7F7";
+}
+
+clickableButtonPressed = function() {
+  // these clickables are ones that change your state
+  // so they route to the adventure manager to do this
+  adventureManager.clickablePressed(this.name); 
 }
